9e82c0aff95a83b05728ba3693d6ce7438a238a6
ArseniyKhal <asmn@mail.ru>
2024-03-29 19:26:00 +0500

    1 section firstscreen

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,39 @@ buttonUpElement.addEventListener("click", () => window.scrollTo({
 	behavior: 'smooth',
 }))
 
+// слайдер
+// const slider = document.querySelector('.slider');
+// const slides = document.querySelector('.slides');
+// const slideItems = document.querySelectorAll('.slide');
+// const slideWidth = slideItems[0].clientWidth;
+// const slidesLength = slideItems.length;
+
+// let currentIndex = 0;
+
+// function slideNext() {
+// 	currentIndex++;
+// 	slides.style.transition = "transform 0.5s ease";
+// 	slides.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+
+// 	if (currentIndex >= slidesLength - 2) {
+// 		slides.addEventListener('transitionend', function () {
+// 			if (currentIndex >= slidesLength - 1) {
+// 				slides.style.transition = "none";
+// 				slides.style.transform = `translateX(0)`;
+// 				currentIndex = 0;
+// 			}
+// 		});
+// 	}
+// }
+
+// function slidePrev() {
+// 	if (currentIndex <= 0) {
+// 		slides.style.transition = "none";
+// 		slides.style.transform = `translateX(-${slideWidth * (slidesLength - 1)}px)`;
+// 		currentIndex = slidesLength - 2;
+// 	} else {
+// 		currentIndex--;
+// 		slides.style.transition = "transform 0.5s ease";
+// 		slides.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+// 	}
+// }
